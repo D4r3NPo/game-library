58187643f8af77236676ef88c2e9a2cb851b0f42
D4r3N <enzo.suares@icloud.com>
2026-04-04 22:26:19 +0200

    serialized game enum as string

--- GameLibraryApi/Models/Game.cs
+++ GameLibraryApi/Models/Game.cs
@@ -1,52 +1,56 @@
+using System.Text.Json.Serialization;
+
 namespace GameLibraryApi.Models;
-using System.ComponentModel.DataAnnotations;
 
 public class Game
 {
-    public string Id { get; set; } = "";
-    public string Title { get; set; } = "";
-    public Platform[] Platforms { get; set; } = [];
-    public Genre[] Genres { get; set; } = [];
-    public Status Status { get; set; } = Status.ToDo;
-    public int? Rating { get; set; } = null;
+	public string Id { get; set; } = "";
+	public string Title { get; set; } = "";
+	public Platform[] Platforms { get; set; } = [];
+	public Genre[] Genres { get; set; } = [];
+	public Status Status { get; set; } = Status.ToDo;
+	public int? Rating { get; set; } = null;
 
-    public override bool Equals(object? obj)
-    {
-        return obj is Game game &&
-            Id == game.Id &&
-            Title == game.Title &&
-            Platforms.SequenceEqual(game.Platforms) &&
-            Genres.SequenceEqual(game.Genres) &&
-            Status == game.Status &&
-            Rating == game.Rating;
-    }
+	public override bool Equals(object? obj)
+	{
+		return obj is Game game &&
+			Id == game.Id &&
+			Title == game.Title &&
+			Platforms.SequenceEqual(game.Platforms) &&
+			Genres.SequenceEqual(game.Genres) &&
+			Status == game.Status &&
+			Rating == game.Rating;
+	}
 
-    public override int GetHashCode() => HashCode.Combine(Id, Title, Platforms, Genres, Status, Rating);
+	public override int GetHashCode() => HashCode.Combine(Id, Title, Platforms, Genres, Status, Rating);
 }
 
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum Platform
 {
-    MacOS,
-    Linux,
-    Windows,
-    iOS,
-    Android,
+	MacOS,
+	Linux,
+	Windows,
+	iOS,
+	Android,
 }
 
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum Status
 {
-    ToDo,
-    InProgress,
-    Done
+	ToDo,
+	InProgress,
+	Done
 }
 
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum Genre
 {
-    RPG,
-    Factory,
-    Sandbox,
-    OpenWorld,
-    Dungeon,
-    Multiplayer,
-    FPS
+	RPG,
+	Factory,
+	Sandbox,
+	OpenWorld,
+	Dungeon,
+	Multiplayer,
+	FPS
 }
